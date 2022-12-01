038f8e73982f4790292b9605a3ab3f23abf5977a
EliFiS <66335691+EliFiS@users.noreply.github.com>
2022-12-01 17:48:41 -0300

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 22250695
-API_HASH = "6b95376fb49d377f70206b3f1a7ec491"
-BOT_TOKEN = "5086036634:AAFpRFQV4NEIzg8fEotiOadKOWIqQQl6rQE"
-SESSION = "AQC2AgVSlkYEhnBSqnYWhTOP0mTN0XSjg2zArOsn3-EmFoUykx5pHxCAsOkiU5Krtd4SEbRypaJGDtvMciKRNTTj3uDrw7jBYM4GHD3PJZUyxO3AsxtG9v6JMrLFfT_bK5prnaVF8qeURtRUVtNXsIWCCa2Qa4A1WhWFJswO9cBu7Xt3sE9O15AVXyLEvzHpdpIPOF9_XgKz4l_YKpwLR-_E489R8tnnoVEMKx77eVHN9RUlO9pNa7ScgfXIitfHw4snczcKhO5QLYdoAoXDzvwj9N_UZU195NkmilRmiBMIboUIdKOySBHwZsqXVEPCPNF09xFZ_b-t2fN7h8eGvnsCAAAAAVfGba8A"
-FORCESUB = "EliFiS_Official_SaveRestricted"
-AUTH = 593171886
+API_ID = 
+API_HASH = ""
+BOT_TOKEN = ""
+SESSION = ""
+FORCESUB = ""
+AUTH = 
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
